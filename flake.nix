8f6b50b6e927a87ae2b73af998ea9876347e9a27
li <li@maisiliym.kriom.me>
2024-06-23 13:41:00 -0600

    (added.lockfile)

--- flake.nix
+++ flake.nix
@@ -1,9 +1,6 @@
 {
   description = "A wrapper flake for lojix";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    clj-nix.url = "github:jlesquembre/clj-nix";
     lojix = {
       url = "github:sajban/lojix";
       flake = false;
@@ -23,6 +20,7 @@
             name = "sajban/lojix";
             main-ns = "lojix.main";
             version = "bootstrap";
+            lockfile = self + "/deps-lock.json"; # Upstream bug: should support a path
             nativeImage.enable = false;
           }];
         };
